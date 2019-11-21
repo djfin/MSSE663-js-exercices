daf66bbd98faa6f3da7b7d272796e1ff374892d1
djfin <djfinley98@gmail.com>
2019-11-21 08:55:56 -0700

    week 3 Observable testing

--- week3/src/observable.spec.ts
+++ week3/src/observable.spec.ts
@@ -1,16 +1,24 @@
 import { expect } from 'chai';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 import 'mocha';
 import {UnderGrad, Class} from './app'
 
-describe('Test for app.ts', () =>{
+describe('Test for observable.ts', () =>{
     const student1 = new UnderGrad("Dan", 2779638);
     const class1 = new Class("CS44", "Fall 2019");
     const student2 = new UnderGrad("Dyl", 2779639);
     var studentsArr = [student1, student2];
     const students = of(studentsArr);
-    var subscribe = students.subscribe(item => console.log(item));
+    it('should assert the students in the right order', () =>{
+        students.subscribe(item => {
+            expect(item).to.equal(studentsArr)
+        });
+    })
     const student3 = new UnderGrad("Gabe", 2779640);
     studentsArr.push(student3);
-    var subscribe = students.subscribe(item => console.log(item));
+    it('should assert the students in the right order', () =>{
+        var student = students.subscribe(item => {
+            expect(item).to.equal(studentsArr)
+        });
+    })
 });
